c9465108cdf049e2ce78e86d819bcc792cff94d0
Richard <54506990+RichLarIva@users.noreply.github.com>
2023-01-12 15:56:38 +0100

    Working with mysql to add support for input

---
+++ ExamWork/ExamWork/PersonClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ExamWork
+{
+	public class PersonClass
+	{
+		private string _fullName;
+		private string _barcode;
+		private bool _isFacualty;
+
+		public string FullName { get => _fullName; set => _fullName = value; }
+		public string Barcode { get => _barcode; set => _barcode = value; }
+		public bool IsFacualty { get => _isFacualty; set => _isFacualty = value; }
+	}
+}
+
--- ExamWork/ExamWork/Program.cs
+++ ExamWork/ExamWork/Program.cs
@@ -6,20 +6,61 @@ class Program
 {
     static MySqlConnection conn;
 
-    public static void Connect(string user, string password)
+
+    public static void Query(string query, MySqlConnection connection)
+    {
+        MySqlCommand command = new MySqlCommand(query, connection);
+        command.CommandTimeout = 60;
+    }
+
+    public static void Connect(string user, string password, string db)
     {
         conn = new MySqlConnection();
         try
         {
-            string connectionString = "datasource=127.0.0.1;port=";
+            string connectionString = $"datasource=127.0.0.1;port=3306;username={user};password={password};database={db}";
+            conn.ConnectionString = connectionString;
+            conn.Open();
+            Console.WriteLine($"Successfully connected to {db}");
+
         }
-        catch(Exception err)
+        catch (Exception err)
         {
-            Console.WriteLine(err.ToString);
+            Console.WriteLine(err.ToString());
         }
+    }
 
     static void Main(string[] args)
     {
+        string query = "SELECT * FROM Persons";
+        Connect("root", "", "MatsalApplikation");
+        MySqlCommand cmd = new MySqlCommand(query, conn);
+        cmd.CommandTimeout = 60;
+        MySqlDataReader reader;
+        try
+        {
+            reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    string[] row = { reader.GetString(0), reader.GetString(1), reader.GetString(2) };
+                    Console.WriteLine(reader.GetString(2));
+                }
+            }
+            else
+            {
+                Console.WriteLine("No rows found.");
+            }
+
+            conn.Close();
+        }
+        catch(Exception err)
+        {
+            Console.WriteLine(err.ToString());
+        }
+
         Console.WriteLine("Hello, World!");
         Console.ReadKey();
     }
