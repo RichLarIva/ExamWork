54eba10cc5a2785e4ba99d3e2ae5486ed0070ae3
Richard <54506990+RichLarIva@users.noreply.github.com>
2023-01-12 15:14:35 +0100

    init

---
+++ ExamWork/ExamWork/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace ExamWork;
+class Program
+{
+    static MySqlConnection conn;
+
+    public static void Connect(string user, string password)
+    {
+        conn = new MySqlConnection();
+        try
+        {
+            string connectionString = "datasource=127.0.0.1;port=";
+        }
+        catch(Exception err)
+        {
+            Console.WriteLine(err.ToString);
+        }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.ReadKey();
+    }
+}
+
